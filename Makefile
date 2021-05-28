2fa705878aebfb3f250bb10a90b1baefc789e839
Anders Kaare <anders@straadt.dk>
2021-05-28 18:50:52 +0200

    small test case
    just an AND gate built of NORs. it currently doesn't compile correctly, so it's
    easier to fix...

--- Makefile
+++ Makefile
@@ -2,14 +2,16 @@
 OPT=-O0 -g
 CFLAGS=-std=c99 -Wall $(OPT) -DDEBUG -DVERBOSE_DEBUG
 
-bin=t1zvm
+bin=t1zvm t2zvm
 
 all: $(bin)
 
 zvm.o: zvm.c zvm.h
 t1zvm.o: t1zvm.c zvm.h
+t2zvm.o: t2zvm.c zvm.h
 
 t1zvm: t1zvm.o zvm.o
+t2zvm: t2zvm.o zvm.o
 
 clean:
 	rm -f *.o $(bin)
---
+++ t2zvm.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "zvm.h"
+
+static uint32_t emit_and()
+{
+	zvm_begin_module(2, 1);
+	struct zvm_pi i0 = zvm_op_input(0);
+	struct zvm_pi i1 = zvm_op_input(1);
+	zvm_op_output(0, zvm_op_nor(zvm_op_nor(i0, i0), zvm_op_nor(i1, i1)));
+	return zvm_end_module();
+}
+
+int main(int argc, char** argv)
+{
+	zvm_init();
+	zvm_begin_program();
+	zvm_end_program(emit_and());
+	return EXIT_SUCCESS;
+}
+
